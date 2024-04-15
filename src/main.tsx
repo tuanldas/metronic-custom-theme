7cab2af478e1ce7c163034cf89835daf98178c96
tuanldas <tuanldas@gmail.com>
2024-04-15 16:32:38 +0700

    Import file css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+
+import './_metronic/sass/style.scss'
+import './_metronic/assets/keenicons/duotone/style.css'
+import './_metronic/assets/keenicons/outline/style.css'
+import './_metronic/assets/keenicons/solid/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
